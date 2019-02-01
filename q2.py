1ebdddfd83f5e91dd6a07d294459a27a6441e08e
hugemerge <hugemerge@gmail.com>
2019-02-01 16:27:49 +0900

    WIP answer question #2

--- q2.py
+++ q2.py
@@ -11,3 +11,19 @@ https://qiita.com/NotFounds/items/4707bacd5934f6191c9d#%E5%95%8F%E9%A1%8C%E6%A6%
 [output]
 N
 """
+
+# Answer
+
+# input
+containers = map(int, input().split())
+
+def dfs(start):
+    que = [[start, 0, 0]]
+    while que:
+        path = que.pop()
+        now = path[-1]
+        if now == int(start / 2):
+            print(path)
+
+        for n in containers:
+            que.append(n)
