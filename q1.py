e362e542c8acd6e39fd2ab5807d26bccc675b9d3
hugemerge <hugemerge@gmail.com>
2019-02-01 15:58:56 +0900

    Answer to #1

--- q1.py
+++ q1.py
@@ -9,7 +9,7 @@
 2行目以降に入力される文字は s/g/0/1 のいずれかであり、他の文字が入力されることは考慮しなくて良い。
 
 [出力]
-最短歩数を一行で出力する。
+最短歩数を一行で出力する。(>> 経路を出力)
 ただしゴールにたどり着けない場合は「-1」を出力する。
 
 [example]
@@ -21,3 +21,47 @@ s 0 0 0 0
 0 0 0 0 0
 0 0 1 0 0
 """
+
+# Answer
+
+# input
+width, height = map(int, input().split())
+vertex = []
+for _ in range(height):
+    for v in input().split():
+        vertex.append(v)
+
+start = vertex.index("s")
+goal = vertex.index("g")
+
+def move_to(i):
+    available = []
+    if i < width * (height-1):
+        available.append(i + width)
+    if width <= i:
+        available.append(i - width)
+    if i % width != 0:
+        available.append(i - 1)
+    if i % width != width-1:
+        available.append(i + 1)
+
+    return available
+
+def bfs(s):
+    que = [[s]]
+    while que:
+        path = que.pop(0)
+        now = path[-1]
+        if now == goal:
+            print(path)
+            return
+
+        for n in move_to(now):
+            if vertex[n] == "1":
+                continue
+            if n not in path:
+                que.append(path + [n])
+
+    print(-1)
+
+bfs(start)
