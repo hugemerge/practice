7aea03ffe27a474719ecdf85fdd14da78960b984
hugemerge <hugemerge@gmail.com>
2019-02-01 11:03:46 +0900

    Add a file

---
+++ dfs_non_recursive.py
@@ -0,0 +1,37 @@
+def dfs_recursive(graph, vertex, path=[]):
+    path += [vertex]
+
+    for neighbor in graph[vertex]:
+        if neighbor not in path:
+            path = dfs_recursive(graph, neighbor, path)
+
+    return path
+
+
+adjacency_matrix = {1: [2, 3], 2: [4, 5],
+                    3: [5], 4: [6], 5: [6],
+                    6: [7], 7: []}
+
+print(dfs_recursive(adjacency_matrix, 1))
+# [1, 2, 4, 6, 7, 5, 3]
+
+def dfs_iterative(graph, start):
+    stack, path = [start], []
+
+    while stack:
+        vertex = stack.pop()
+        if vertex in path:
+            continue
+        path.append(vertex)
+        for neighbor in graph[vertex]:
+            stack.append(neighbor)
+
+    return path
+
+
+adjacency_matrix = {1: [2, 3], 2: [4, 5],
+                    3: [5], 4: [6], 5: [6],
+                    6: [7], 7: []}
+
+print(dfs_iterative(adjacency_matrix, 1))
+# [1, 3, 5, 6, 7, 2, 4]
